89445b3ea674dba47e37d5cee01511f1c15b3afe
om vrit <omvritoo@gmail.com>
2023-05-04 14:59:40 +0530

    updated task 3

---
+++ Batch_22-26/Task-3/om_vrit_cse/IndiaMonument/app/src/main/java/com/example/indiamonument/hawamahal.kt
@@ -0,0 +1,11 @@
+package com.example.indiamonument
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class hawamahal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hawamahal)
+    }
+}
---
+++ Batch_22-26/Task-3/om_vrit_cse/IndiaMonument/app/src/main/java/com/example/indiamonument/tajmahal.kt
@@ -0,0 +1,11 @@
+package com.example.indiamonument
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class tajmahal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tajmahal)
+    }
+}
